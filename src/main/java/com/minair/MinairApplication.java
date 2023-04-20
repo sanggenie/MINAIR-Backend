eaefa0042d0f1227c386dc9a910d12153ec39f0c
leesangjin <tkdwls0628@gmail.com>
2023-04-20 21:50:14 +0900

    [Chore] Initial Setting

---
+++ src/main/java/com/minair/MinairApplication.java
@@ -0,0 +1,13 @@
+package com.minair;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinairApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MinairApplication.class, args);
+	}
+
+}
