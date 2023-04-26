6962da259d273fc4a858d5341fa8c5e8eff45dad
leesangjin <tkdwls0628@gmail.com>
2023-04-27 01:55:14 +0900

    [Refactor]: City 위도,경도 float 타입으로 변경

--- src/main/java/com/minair/domain/City.java
+++ src/main/java/com/minair/domain/City.java
@@ -22,6 +22,6 @@ public class City {
     private Long id;
 
     private String name;
-    private double longitude;
-    private double latitude;
+    private float longitude;
+    private float latitude;
 }
