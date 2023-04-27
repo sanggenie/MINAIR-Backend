39f09f1d3f5591b850dc8a298c57be0604c9f95f
leesangjin <tkdwls0628@gmail.com>
2023-04-27 18:21:30 +0900

    [Style]: 사용안하는 어노테이션 제거

--- src/main/java/com/minair/dto/WeatherInfo.java
+++ src/main/java/com/minair/dto/WeatherInfo.java
@@ -1,12 +1,10 @@
 package com.minair.dto;
 
 import lombok.Getter;
-import lombok.ToString;
 
 import java.util.concurrent.ConcurrentHashMap;
 
 @Getter
-@ToString
 public class WeatherInfo {
 
     private ConcurrentHashMap<String, Object> daily;
