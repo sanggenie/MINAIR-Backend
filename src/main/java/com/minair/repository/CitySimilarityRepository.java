9b25e814b02ee7759a6940216bb41c8812ac438f
leesangjin <tkdwls0628@gmail.com>
2023-06-02 00:47:37 +0900

    [Feat]: 가중치 증가를 위한 조회 쿼리 구현

--- src/main/java/com/minair/repository/CitySimilarityRepository.java
+++ src/main/java/com/minair/repository/CitySimilarityRepository.java
@@ -5,11 +5,18 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.util.Optional;
 import java.util.List;
 
-
-
 public interface CitySimilarityRepository extends JpaRepository<CitySimilarity, Long> {
+
     @Query("SELECT cs FROM CitySimilarity cs JOIN cs.city c WHERE c.id = :cityId")
-    List<CitySimilarity> findByCityId(@Param("cityId") Long cityId);
+    List<CitySimilarity> findAllByCityId(@Param("cityId") Long cityId);
+
+    @Query("select cs from CitySimilarity cs " +
+            "join cs.city c " +
+            "join cs.targetCity tc " +
+            "where c.id = :cityId and tc.id = :targetCityId")
+    Optional<CitySimilarity> findByCityIdAndTargetCityId(@Param("cityId") Long cityId,
+                                                         @Param("targetCityId") Long targetCityId);
 }
