11d61b646b46d7b40f232e57784666f0997f6201
Sungho Lee <dltjdgh69@gmail.com>
2023-06-02 01:30:16 +0900

    [refactor]: 유사 여행지 동일 객체 추출 문제 개선

--- src/main/java/com/minair/service/CitySimilarityService.java
+++ src/main/java/com/minair/service/CitySimilarityService.java
@@ -46,12 +46,12 @@ public class CitySimilarityService {
 
     public List<CitySimilarity> calculateWeights(Long cityid) {
         List<CitySimilarity> cities = findByCityId(cityid);
-
+        
         List<Double> calculatedWeights = new ArrayList<>();
         List<CitySimilarity> result = new ArrayList<>();
         Random random = new Random();
 
-        int count = 2;
+        int count = 5;
 
         double totalWeight = 0.0;
 
@@ -62,25 +62,29 @@ public class CitySimilarityService {
             totalWeight += weight;
         }
 
-
-        for (int i = 0; i < calculatedWeights.size(); i++) {
-            double normalizedWeight = calculatedWeights.get(i) / totalWeight;
-            calculatedWeights.set(i, normalizedWeight);
-        }
-
-
         for (int i = 0; i < count; i++) {
             double randomValue = random.nextDouble();
             double cumulativeWeight = 0.0;
+
+            normalizeWeights(calculatedWeights, totalWeight);
 
             for (int j = 0; j < calculatedWeights.size(); j++) {
                 cumulativeWeight += calculatedWeights.get(j);
                 if (randomValue < cumulativeWeight) {
                     result.add(cities.get(j));
+                    calculatedWeights.remove(j);
                     break;
                 }
             }
         }
+
         return result;
+    }
+    public List<Double> normalizeWeights(List<Double> calculatedWeights, double totalWeight){
+        for (int i = 0; i < calculatedWeights.size(); i++) {
+            double normalizedWeight = calculatedWeights.get(i) / totalWeight;
+            calculatedWeights.set(i, normalizedWeight);
+        }
+        return calculatedWeights;
     }
 }
