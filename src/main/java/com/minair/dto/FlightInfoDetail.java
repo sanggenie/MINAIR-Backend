473d79ef1c8e26b457a74823e655d0eb781b7c9f
leesangjin <tkdwls0628@gmail.com>
2023-06-06 16:34:16 +0900

    [Feat]: 항공권 요청시, 필요한 데이터만 파싱하기 위한 DTO 구현

---
+++ src/main/java/com/minair/dto/FlightInfoDetail.java
@@ -0,0 +1,16 @@
+package com.minair.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
+
+@Getter
+public class FlightInfoDetail {
+
+    String flyTo;
+
+    @JsonProperty("local_arrival")
+    String localArrival;
+
+    @JsonProperty("price")
+    float price;
+}
